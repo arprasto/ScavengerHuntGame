35ff3658b210fafa3785851758c7f5fac5f4db9a
arprasto <arprasto@in.ibm.com>
2017-06-19 13:40:30 +0530

    removing my cred

--- src/main/java/com/ibm/watson/scavenger/util/ScavengerContants.java
+++ src/main/java/com/ibm/watson/scavenger/util/ScavengerContants.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 
 public class ScavengerContants 
 {
-	public static String TTS_uname = "13719587-bb88-49de-a558-508df4ff0674",
-			TTS_pass = "Gf161N5tk3eN",
-			STT_uname = "7077f074-95e0-47fb-85b1-f985fd4ffc24",
-			STT_pass = "GzZ0NhIb2oaH",
-			ImageRecogAPIKey = "8481419871c4b6ec79a27dda81f436f109870d65",
+	public static String TTS_uname = "xxxx",
+			TTS_pass = "XXXX",
+			STT_uname = "xxxx",
+			STT_pass = "xxxx",
+			ImageRecogAPIKey = "xxxx",
 			tmp_image_dir_path = "/tmp/Watson";
 	
 	public static File tmp_image_dir = null;
